ea7839573d8c0a796233afb4d666403e6936e480
raulscr <raulsfreitas1997@hotmail.com>
2023-10-03 21:35:26 -0300

    feat: add gradle/kotlin project

---
+++ src/main/kotlin/com/example/KotlinRinhaBackend/KotlinRinhaBackendApplication.kt
@@ -0,0 +1,10 @@
+package com.example.KotlinRinhaBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class KotlinRinhaBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinRinhaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/KotlinRinhaBackend/KotlinRinhaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.KotlinRinhaBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRinhaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

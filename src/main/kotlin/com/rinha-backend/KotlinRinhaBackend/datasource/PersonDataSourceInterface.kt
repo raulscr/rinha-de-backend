ca171fdc91587361ddb3bc00898378bd0ed71407
raulscr <raulsfreitas1997@hotmail.com>
2023-10-10 00:28:59 -0300

    add PersonModel dependency on PersonDataSourceInterface

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/datasource/PersonDataSourceInterface.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/datasource/PersonDataSourceInterface.kt
@@ -1,5 +1,7 @@
 package com.rinha.backend.KotlinRinhaBackend.datasource
 
+import com.rinha.backend.KotlinRinhaBackend.model.PersonModel
+
 interface PersonDataSourceInterface {
     fun getPersonById(id: String): Collection<PersonModel>
 }

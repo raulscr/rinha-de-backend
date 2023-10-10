7e3adba5aff12a92ca2eefea4f9a9a87640f22ed
raulscr <raulsfreitas1997@hotmail.com>
2023-10-10 15:51:07 -0300

    feat: add PersonDataSourceInterface lacking abstract methods

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/datasource/PersonDataSourceInterface.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/datasource/PersonDataSourceInterface.kt
@@ -4,4 +4,7 @@ import com.rinha.backend.KotlinRinhaBackend.model.PersonModel
 
 interface PersonDataSourceInterface {
     fun getPersonById(id: String): Collection<PersonModel>
+    fun getPersonByFilter(filter: String): Collection<PersonModel>
+    fun insertPerson(people: Collection<PersonModel>)
+    fun getCountPeople(): Int
 }

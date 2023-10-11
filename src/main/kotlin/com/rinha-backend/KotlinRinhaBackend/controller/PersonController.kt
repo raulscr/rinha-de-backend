7f3b8f226b17a283fdf0419196b2bc65a00f0f17
raulscr <raulsfreitas1997@hotmail.com>
2023-10-11 00:42:57 -0300

    fix: implement all getMapping on controller

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/controller/PersonController.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/controller/PersonController.kt
@@ -1,39 +1,39 @@
 package com.rinha.backend.KotlinRinhaBackend.controller
 
+import com.rinha.backend.KotlinRinhaBackend.model.PersonModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import com.rinha.backend.KotlinRinhaBackend.service.PersonService
 
 @RestController
 @RequestMapping("")
-class PersonController {
+class PersonController(private val service: PersonService) {
+
     @PostMapping("pessoas")
     fun insertPerson(): String {
         return "insertPerson"
     }
 
     @GetMapping("pessoas/{id}")
-    fun searchPersonById(@PathVariable id: String): String {
-        // @TODO: use a single function to filter by whatever it is (id, blablabla), if it's even
-        // possible
-        println(id)
-        return "searchPersonById"
+    fun searchPersonById(@PathVariable id: String): PersonModel {
+        return service.getPersonById(id)
     }
 
     @GetMapping("pessoas/")
-    fun searchPersonByFilter(@RequestParam("t") filter: String?): String {
+    fun searchPersonByFilter(@RequestParam("t") filter: String?): Collection<PersonModel>  {
         if (filter == null) {
-            return "400 - bad request"
+            return listOf()
         }
 
-        return "searchPersonByFilter $filter"
+        return service.getPersonByFilter(filter)
     }
 
     @GetMapping("contagem-pessoas")
-    fun contagemPessoas(): String {
-        return "contagemPessoas"
+    fun contagemPessoas(): Int {
+        return service.getCountPeople()
     }
 }

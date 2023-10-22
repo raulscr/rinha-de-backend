a6b956579e016e8bd418f0a3683bbbf0f07e9f9d
raulscr <raulsfreitas1997@hotmail.com>
2023-10-22 13:45:57 -0300

    refactor: update service to return optional value on getPersonById receive single PersonModel on insertPerson

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/service/PersonService.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/service/PersonService.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class PersonService(private val dataSource: PersonDataSourceInterface) {
-    fun getPersonById(id: Long): PersonModel {
+    fun getPersonById(id: Long): PersonModel? {
         return dataSource.getPersonById(id)
     }
 
@@ -14,8 +14,8 @@ class PersonService(private val dataSource: PersonDataSourceInterface) {
         return dataSource.getPersonByFilter(filter)
     }
 
-    fun insertPerson(people: Collection<PersonModel>) {
-        return dataSource.insertPerson(people)
+    fun insertPerson(person: PersonModel) {
+        return dataSource.insertPerson(person)
     }
 
     fun getCountPeople(): Int {

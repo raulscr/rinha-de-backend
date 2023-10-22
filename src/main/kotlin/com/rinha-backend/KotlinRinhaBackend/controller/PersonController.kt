7241916593a2197cca265aff39b603ac032f1fb7
raulscr <raulsfreitas1997@hotmail.com>
2023-10-22 13:46:44 -0300

    refactor: handle optional value on getPersonById receive single PersonModel on insertPerson

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/controller/PersonController.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/controller/PersonController.kt
@@ -3,6 +3,7 @@ package com.rinha.backend.KotlinRinhaBackend.controller
 import com.rinha.backend.KotlinRinhaBackend.model.PersonModel
 import com.rinha.backend.KotlinRinhaBackend.service.PersonService
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,12 +13,14 @@ class PersonController(private val service: PersonService) {
     @PostMapping("pessoas")
     @ResponseStatus(HttpStatus.CREATED)
     fun insertPerson(@RequestBody person: PersonModel) {
-        service.insertPerson(listOf(person))
+        service.insertPerson(person)
     }
 
     @GetMapping("pessoas/{id}")
-    fun searchPersonById(@PathVariable id: String): PersonModel {
-        return service.getPersonById(id.toLong())
+    fun searchPersonById(@PathVariable id: String): ResponseEntity<PersonModel> {
+        val result = service.getPersonById(id.toLong()) ?: return ResponseEntity.notFound().build()
+
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("pessoas/")

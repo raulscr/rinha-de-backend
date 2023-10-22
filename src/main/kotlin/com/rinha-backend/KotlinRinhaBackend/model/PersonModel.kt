ac71d4c405667cd337627daf878a089a4ed940c7
raulscr <raulsfreitas1997@hotmail.com>
2023-10-22 10:23:30 -0300

    refactor: add database annotations to PersonModel and change id to Long

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/model/PersonModel.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/model/PersonModel.kt
@@ -1,9 +1,23 @@
 package com.rinha.backend.KotlinRinhaBackend.model
 
+import jakarta.annotation.Generated
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Table(name = "person")
+@Entity
 data class PersonModel(
-                val id: String,
-                val name: String,
-                val nickname: String,
-                val bornDate: String,
-                val stack: Collection<String>
+    @Id
+    @Generated()
+    @Column(name = "id")
+    val id: Long,
+    @Column(name = "name")
+    val name: String,
+    @Column(name = "nick_name")
+    val nickname: String,
+    @Column(name = "born_date")
+    val bornDate: String,
+    val stack: Collection<String>
 )

a0d2a117264612872e6bdefa4594a3e3254c2407
raulscr <raulsfreitas1997@hotmail.com>
2023-10-22 13:40:35 -0300

    refactor: remove rowmapper from dataclass PersonModel, and update attributes id and stack to be optional

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/model/PersonModel.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/model/PersonModel.kt
@@ -1,23 +1,9 @@
 package com.rinha.backend.KotlinRinhaBackend.model
 
-import jakarta.annotation.Generated
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
-
-@Table(name = "person")
-@Entity
 data class PersonModel(
-    @Id
-    @Generated()
-    @Column(name = "id")
-    val id: Long,
-    @Column(name = "name")
+    val id: Long? = null,
     val name: String,
-    @Column(name = "nick_name")
-    val nickname: String,
-    @Column(name = "born_date")
+    val nickName: String,
     val bornDate: String,
-    val stack: Collection<String>
+    val stack: Collection<String>? = null
 )

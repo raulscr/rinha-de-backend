caae79abb229201fded70d297f3a2a449534d544
raulscr <raulsfreitas1997@hotmail.com>
2023-10-23 21:14:36 -0300

    feat: refactor queries

--- src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/datasource/repository/PersonRepository.kt
+++ src/main/kotlin/com/rinha-backend/KotlinRinhaBackend/datasource/repository/PersonRepository.kt
@@ -13,7 +13,18 @@ import java.sql.ResultSet
 class PersonRepository(private val jdbcTemplate: JdbcTemplate) : PersonDataSourceInterface {
 
     override fun getPersonById(@Param("id") id: Long): PersonModel? {
-        val sqlQuery: String = "SELECT p.id, p.name, p.nick_name, p.born_date FROM person p WHERE p.id = ?;"
+        val sqlQuery: String = """
+            SELECT
+                p.id,
+                p.name,
+                p.nick_name,
+                p.born_date
+            FROM
+                person p
+            WHERE (
+                p.id = ?
+            );
+        """
         return jdbcTemplate.query(sqlQuery, personRowMapper, id).firstOrNull();
     }
 
@@ -22,13 +33,29 @@ class PersonRepository(private val jdbcTemplate: JdbcTemplate) : PersonDataSourc
     }
 
     override fun insertPerson(person: PersonModel) {
-        val sqlQuery: String =
-            "INSERT INTO person (name, nick_name, born_date) VALUES (?, ?, ?);"
+        val sqlQuery: String = """
+            INSERT INTO
+                person (
+                    name,
+                    nick_name,
+                    born_date
+                ) VALUES 
+                (
+                    ?,
+                    ?,
+                    ?
+                );
+        """
         jdbcTemplate.update(sqlQuery, person.name, person.nickName, person.bornDate)
     }
 
     override fun getCountPeople(): Int {
-        val sqlQuery: String = "SELECT COUNT(*) AS count FROM person;"
+        val sqlQuery: String = """
+            SELECT 
+                COUNT(*) AS count 
+            FROM
+                person;
+        """
         return jdbcTemplate.query(sqlQuery, RowMapper<Int> { rs: ResultSet, _: Int ->
             rs.getInt("count")
         }).first()

1ffeb347459adc84d65bf9911c12159f6cbdf0be
raulscr <raulsfreitas1997@hotmail.com>
2023-10-23 23:16:48 -0300

    refactor: change db schema

--- src/main/resources/db/migration/V2__CREATE_PERSON_STACK.sql
+++ src/main/resources/db/migration/V2__CREATE_PERSON_STACK.sql
@@ -1,12 +1,6 @@
-DROP TABLE IF EXISTS stack;
-CREATE TABLE stack (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    stack VARCHAR(32)
-);
-
 DROP TABLE IF EXISTS person_stack;
 CREATE TABLE person_stack (
-    person_id INT,
-    stack_id INT,
-    CONSTRAINT PRIMARY KEY (person_id, stack_id)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    person_id INT NOT NULL,
+    stack VARCHAR(32)
 );
